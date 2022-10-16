6fb5b094034000d404f11d20656bdbdbafcc95f7
Renz Linga <91326461+KuantumuvGravitav@users.noreply.github.com>
2022-10-16 13:35:24 +0800

    Update Linear_Diophantine_Equation.m

--- Linear_Diophantine_Equation.m
+++ Linear_Diophantine_Equation.m
@@ -1,3 +1,8 @@
+% Problem:
+% A merchant has three items on sale, namely a radio for P50, a clock for P30, and a flashlight for P1.
+% At the end of the day, he sold a total of 100 of the three items and has taken exactly P1,000 on the total sales.
+% How many radios did he sell? (MATLAB solution)
+
 function items = benta(sold, earned)
 % sold = 100
 % earned = 1000
